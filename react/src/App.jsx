1ebe9e4fb84caa05c419976e8f46531953a070a9
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-20 18:03:38 +0530

    Create updateHelpshiftConfig to update the config object after page is loaded

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,9 +2,14 @@
 
 function App() {
 
+  const updateHelpshiftConfig=()=>{
+    window.Helpshift("updateHelpshiftConfig");
+  }
+
   return (
     <>
       <h1>Webchat demo</h1>
+      <button onClick={updateHelpshiftConfig}>Update Helpshift Config</button>
     </>
   )
 }
