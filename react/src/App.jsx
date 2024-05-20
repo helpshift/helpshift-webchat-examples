0054f480e52e7564ae6990800077f06599f28eaa
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-20 18:09:07 +0530

    Integrate full privacy option in config object

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,29 @@
-
-
 function App() {
-
-  const updateHelpshiftConfig=()=>{
+  const updateHelpshiftConfig = () => {
     window.Helpshift("updateHelpshiftConfig");
-  }
+  };
+
+  const enablePrivacy = () => {
+    window.helpshiftConfig.fullPrivacy = true;
+    updateHelpshiftConfig();
+  };
+
+  const disablePrivacy = () => {
+    window.helpshiftConfig.fullPrivacy = false;
+    updateHelpshiftConfig();
+  };
 
   return (
     <>
       <h1>Webchat demo</h1>
       <button onClick={updateHelpshiftConfig}>Update Helpshift Config</button>
+      <div>
+        <h3>Privacy Options</h3>
+        <button onClick={enablePrivacy}>Enable Privacy</button>
+        <button onClick={disablePrivacy}>Disable Privacy</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
