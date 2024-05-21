3954838a9dbbdb8106711f2b230ad9200b170452
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-21 15:51:11 +0530

    Integrate various widget options like + Enable fullscreen mode + Hide and show the launcher + Position of widget

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 function App() {
+  const [position, setPosition] = useState("bottom-right");
+
   const updateHelpshiftConfig = () => {
     window.Helpshift("updateHelpshiftConfig");
   };
@@ -19,6 +23,21 @@ function App() {
     updateHelpshiftConfig();
   };
 
+  const onChangeHandler = (e) => {
+    const value = e.target.value;
+    setPosition(value);
+  };
+
+  const handleWidgetPosition = () => {
+    window.helpshiftConfig.widgetOptions.position = position;
+    updateHelpshiftConfig();
+  };
+
+  const enterFullScreen = () => {
+    window.helpshiftConfig.widgetOptions.fullScreen = true;
+    updateHelpshiftConfig();
+  };
+
   return (
     <>
       <h1>Webchat demo</h1>
@@ -33,6 +52,41 @@ function App() {
         <h3>Privacy Options</h3>
         <button onClick={enablePrivacy}>Enable Privacy</button>
         <button onClick={disablePrivacy}>Disable Privacy</button>
+      </div>
+
+      <div>
+        <h3>Widget options</h3>
+        <ul>
+          <li>
+            <div>
+              <h5>Launcher Options</h5>
+              <button onClick={() => window.Helpshift("show")}>
+                Show launcher
+              </button>
+              <button onClick={() => window.Helpshift("hide")}>
+                Hide launcher
+              </button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h4>Select Position</h4>
+              <select value={position} onChange={onChangeHandler}>
+                <option value="bottom-right">bottom-right</option>
+                <option value="bottom-left">bottom-left</option>
+                <option value="top-left">top-left</option>
+                <option value="top-right">top-right</option>
+              </select>
+              <button onClick={handleWidgetPosition}>Apply</button>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h5>Fullscreen Mode</h5>
+              <button onClick={enterFullScreen}>Enter full screen</button>
+            </div>
+          </li>
+        </ul>
       </div>
     </>
   );
