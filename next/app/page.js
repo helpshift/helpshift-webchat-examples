34d12e9d583e80b2c7d12e95456758de1b4a7418
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-31 02:38:09 +0530

    Create a client component webChatOptions.jsx to add user interactivity

--- next/app/page.js
+++ next/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import WebChatOptions from "./webChatOptions";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Web Chat
+    <main className="max-w-3xl mt-5">
+      <h1 className="text-5xl text-center">Web Chat</h1>
+      <WebChatOptions></WebChatOptions>
     </main>
   );
 }
---
+++ next/app/webChatOptions.jsx
@@ -0,0 +1,8 @@
+"use client";
+import React from "react";
+
+const WebChatOptions = () => {
+  return <div>WebChatOptions</div>;
+};
+
+export default WebChatOptions;
