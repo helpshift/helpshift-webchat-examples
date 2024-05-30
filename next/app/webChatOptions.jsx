548fad1b190af046f3455653b5c6d2986f513e34
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-31 02:45:32 +0530

    Create updateHelpshiftConfig to update the config object after page is loaded

--- next/app/webChatOptions.jsx
+++ next/app/webChatOptions.jsx
@@ -2,7 +2,25 @@
 import React from "react";
 
 const WebChatOptions = () => {
-  return <div>WebChatOptions</div>;
+  return (
+    <>
+      <div className="w-full border-gray-300 mt-12 rounded-lg">
+        <h3 className="py-2 pl-3 text-lg rounded-t-lg font-bold uppercase text-gray-800">
+          Quick setup
+        </h3>
+        <div className="m-3">
+          <div className="mt-2 flex justify-between">
+            <h4 className="text-lg py-2">Update config</h4>
+            <div>
+              <button onClick={() => window.Helpshift("updateHelpshiftConfig")}>
+                Update Helpshift Config
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WebChatOptions;
