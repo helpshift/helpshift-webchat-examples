9b5131e5907d8cf461b73733acb796b3f631cb36
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-31 02:56:03 +0530

    Integrate enable full privacy option in the config object

--- next/app/webChatOptions.jsx
+++ next/app/webChatOptions.jsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 const WebChatOptions = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [selectedPrivacyOption, setSelectedPrivacyOption] = useState("disable");
+
   const onLogin = () => {
     window.helpshiftConfig.userId = "captain_planet12";
     window.helpshiftConfig.userEmail = "captain@example.com";
@@ -16,6 +18,17 @@ const WebChatOptions = () => {
     window.helpshiftConfig.userEmail = "";
     window.Helpshift("updateHelpshiftConfig");
     setIsLoggedIn(false);
+  };
+
+  const onFullPrivacyChange = (e) => {
+    const val = e.target.value;
+    setSelectedPrivacyOption(val);
+    if (val === "enable") {
+      window.helpshiftConfig.fullPrivacy = true;
+    } else {
+      window.helpshiftConfig.fullPrivacy = false;
+    }
+    window.Helpshift("updateHelpshiftConfig");
   };
 
   return (
@@ -33,6 +46,7 @@ const WebChatOptions = () => {
               </button>
             </div>
           </div>
+
           <div className="mt-2 flex justify-between">
             <h4 className="text-lg py-2">Login user</h4>
             <div>
@@ -43,6 +57,38 @@ const WebChatOptions = () => {
               )}
             </div>
           </div>
+          <div className="mt-2 flex justify-between">
+            <h4 className="text-lg py-2">Full privacy</h4>
+            <div className="lex flex-col space-y-2 p-2">
+              <form>
+                <label className="inline-flex items-center mr-4">
+                  <input
+                    type="radio"
+                    value={"enable"}
+                    checked={selectedPrivacyOption === "enable"}
+                    onChange={onFullPrivacyChange}
+                    className="form-radio h-5 w-5 text-blue-600"
+                  ></input>
+                  <span className="ml-2 text-gray-700">
+                    Enable Full Privacy
+                  </span>
+                </label>
+                <label className="inline-flex items-center">
+                  <input
+                    type="radio"
+                    value={"disable"}
+                    checked={selectedPrivacyOption === "disable"}
+                    onChange={onFullPrivacyChange}
+                    className="form-radio h-5 w-5 text-blue-600"
+                  ></input>
+                  <span className="ml-2 text-gray-700">
+                    Disable Full Privacy
+                  </span>
+                </label>
+              </form>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
