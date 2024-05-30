efe3ac875e6c5b8b373a2faa399948e5aa74ce74
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-05-31 02:50:49 +0530

    Integrate Login functionality + Login the user by setting userId and userEmail in config object

--- next/app/webChatOptions.jsx
+++ next/app/webChatOptions.jsx
@@ -1,7 +1,23 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 
 const WebChatOptions = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const onLogin = () => {
+    window.helpshiftConfig.userId = "captain_planet12";
+    window.helpshiftConfig.userEmail = "captain@example.com";
+    window.Helpshift("updateHelpshiftConfig");
+    setIsLoggedIn(true);
+  };
+
+  const onLogout = () => {
+    window.helpshiftConfig.userId = "";
+    window.helpshiftConfig.userEmail = "";
+    window.Helpshift("updateHelpshiftConfig");
+    setIsLoggedIn(false);
+  };
+
   return (
     <>
       <div className="w-full border-gray-300 mt-12 rounded-lg">
@@ -15,6 +31,16 @@ const WebChatOptions = () => {
               <button onClick={() => window.Helpshift("updateHelpshiftConfig")}>
                 Update Helpshift Config
               </button>
+            </div>
+          </div>
+          <div className="mt-2 flex justify-between">
+            <h4 className="text-lg py-2">Login user</h4>
+            <div>
+              {isLoggedIn ? (
+                <button onClick={onLogout}>Logout</button>
+              ) : (
+                <button onClick={onLogin}>Login</button>
+              )}
             </div>
           </div>
         </div>
