377c8e81894b765f294f86ad6c269eb8cd0e8dfa
dhruv_gogna <dhruv.gogna@helpshift.com>
2024-06-24 14:02:06 +0530

    Add web chat embed code in the root component

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,3 +1,50 @@
-<template>
-  Web Chat
-</template>
+<template>Web Chat</template>
+
+<script setup>
+
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const script = document.createElement("script");
+  script.text = `
+    (function () {
+
+      var PLATFORM_ID = "${import.meta.env.VITE_APP_PLATFORM_ID}",
+        DOMAIN = "${import.meta.env.VITE_APP_DOMAIN}",
+        LANGUAGE = "en";
+
+
+      window.helpshiftConfig = {
+        platformId: PLATFORM_ID,
+        domain: DOMAIN,
+        language: LANGUAGE,
+        widgetOptions:{},
+      };
+      })();
+      !(function (t, e) {
+        if ("function" != typeof window.Helpshift) {
+          var n = function () {
+            n.q.push(arguments);
+          };
+          (n.q = []), (window.Helpshift = n);
+          var i,
+            a = t.getElementsByTagName("script")[0];
+          if (t.getElementById(e)) return;
+          (i = t.createElement("script")),
+            (i.async = !0),
+            (i.id = e),
+            (i.src = "https://webchat.helpshift.com/latest/webChat.js");
+          var o = function () {
+            window.Helpshift("init");
+          };
+          window.attachEvent
+            ? i.attachEvent("onload", o)
+            : i.addEventListener("load", o, !1),
+            a.parentNode.insertBefore(i, a);
+        } else window.Helpshift("update");
+      })(document, "hs-chat");
+
+  `;
+  document.body.appendChild(script);
+});
+</script>
